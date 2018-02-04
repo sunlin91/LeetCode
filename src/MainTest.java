0f18ea6ef1bd3a13ffdcea60fcbeb35c185df79d
sunlin91 <407388356@qq.com>
2018-02-04 16:44:48 +0800

    20180204

--- src/MainTest.java
+++ src/MainTest.java
@@ -1,6 +1,7 @@
 public class MainTest {
 
     public static void main(String[] args) {
-
+        U_Q044_WildcardMatching q = new U_Q044_WildcardMatching();
+        System.out.println(q.isMatch("abcdefgcdgg","a*cd*cdg****g"));
     }
 }
