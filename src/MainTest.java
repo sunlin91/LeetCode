471b6c8f090af2264ca206efc310086c6ef6a7fd
sunllin <407388356@qq.com>
2018-02-26 21:48:44 +0800

    2018.2.26

--- src/MainTest.java
+++ src/MainTest.java
@@ -1,7 +1,21 @@
 public class MainTest {
 
     public static void main(String[] args) {
-        Q047_PermutationsII q = new Q047_PermutationsII();
-        q.permuteUnique(new int[]{1,1,1,2,2,3,4,5});
+        int[][] a = new int[][]{{1, 1, 1,1}, {2,2,2,2}, {3,3,3,3},{4,4,4,4}};
+        Q048_RotateImage q = new Q048_RotateImage();
+        for(int i=0;i<a.length;i++){
+            for(int j=0;j<a.length;j++){
+                System.out.print(a[i][j]);
+            }
+            System.out.println();
+        }
+        q.rotate(a);
+        System.out.println("==========");
+        for(int i=0;i<a.length;i++){
+            for(int j=0;j<a.length;j++){
+                System.out.print(a[i][j]);
+            }
+            System.out.println();
+        }
     }
 }
