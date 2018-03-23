8ac288e59f9f8778a6819571290d77e651ae4a84
sunllin <407388356@qq.com>
2018-03-23 10:49:08 +0800

    20180323美团真题

--- src/Main2.java
+++ src/Main2.java
@@ -1,38 +1,5 @@
-import java.util.Scanner;
-
 /**
  * Created by SunLin on 2018.3.22
  */
 public class Main2 {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine().trim();
-        int[] a = new int[10];
-        char[] chars = str.toCharArray();
-        for (int i=0;i<chars.length;i++)
-            a[chars[i]-'0']++;
-        int i=1;
-        while (true){
-            if (!helper(a,i)){
-                System.out.println(i);
-                break;
-            }
-            i++;
-        }
-    }
-    static boolean helper(int[] array,int num){
-        int[] a = new int[array.length];
-        for (int i = 0;i<array.length;i++){
-            a[i]=array[i];
-        }
-        while (num!=0){
-            int tmp = num%10;
-            if (a[tmp]<=0) return false;
-            else {
-                num/=10;
-                a[tmp]--;
-            }
-        }
-        return true;
-    }
 }
---
+++ src/pastExamPaper/meituan/test02.java
@@ -0,0 +1,40 @@
+package pastExamPaper.meituan;
+import java.util.Scanner;
+
+/**
+ * Created by SunLin on 2018.3.23
+ */
+
+public class test02 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine().trim();
+        int[] a = new int[10];
+        char[] chars = str.toCharArray();
+        for (int i=0;i<chars.length;i++)
+            a[chars[i]-'0']++;
+        int i=1;
+        while (true){
+            if (!helper(a,i)){
+                System.out.println(i);
+                break;
+            }
+            i++;
+        }
+    }
+    static boolean helper(int[] array,int num){
+        int[] a = new int[array.length];
+        for (int i = 0;i<array.length;i++){
+            a[i]=array[i];
+        }
+        while (num!=0){
+            int tmp = num%10;
+            if (a[tmp]<=0) return false;
+            else {
+                num/=10;
+                a[tmp]--;
+            }
+        }
+        return true;
+    }
+}
