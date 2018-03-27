893c6dbfb6106e2332a37c0dc9ceee53a75c7768
sunllin <407388356@qq.com>
2018-03-27 19:14:26 +0800

    20180327网易2018实习校招

---
+++ src/pastExamPaper/wangyi/wangyi2018test03.java
@@ -0,0 +1,13 @@
+package pastExamPaper.wangyi;
+
+import java.util.Scanner;
+
+/**
+ * Created by SunLin on 2018.3.27
+ */
+public class wangyi2018test03 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
