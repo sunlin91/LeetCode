36440d089963cdc42532f5d2648d1b9c6e568475
sunllin <407388356@qq.com>
2018-03-27 22:11:19 +0800

    test

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,5 @@
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello");
-
-    }
-    //希尔排序
-    public static void shellSort(int[] a){
-        int i,j,k;
-        for(int dk=a.length/2;dk>=1;dk=dk/2)
-            for(i=dk;i<a.length;i++){
-                if(a[i]<a[i-dk]){
-                    k=a[i];
-                    for(j=i-dk;j>=0&&k<a[j];j-=dk){
-                        a[j+dk]=a[j];
-                    }
-                    a[j+dk]=k;
-                }
-            }
     }
 }
