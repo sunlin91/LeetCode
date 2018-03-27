25c6b2ad30792cb53ee12e9725921a9d90458cfc
sunlin91 <407388356>
2018-03-27 20:48:16 +0800

    test

--- src/pastExamPaper/wangyi/wangyi2018test01.java
+++ src/pastExamPaper/wangyi/wangyi2018test01.java
@@ -8,6 +8,68 @@ import java.util.Scanner;
 public class wangyi2018test01 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] x1 = new int[n];
+        int[] y1 = new int[n];
+        int[] x2 = new int[n];
+        int[] y2 = new int[n];
+        int minx1 = Integer.MAX_VALUE;
+        int miny1 = Integer.MAX_VALUE;
+        int maxx2 = Integer.MIN_VALUE;
+        int maxy2 = Integer.MIN_VALUE;
+        for (int i=0;i<n;i++){
+            x1[i]=sc.nextInt();
+            if (x1[i]<minx1)
+                minx1=x1[i];
+        }
+        for (int i=0;i<n;i++){
+            y1[i]=sc.nextInt();
+            if (y1[i]<miny1)
+                miny1=y1[i];
+        }
+        for (int i=0;i<n;i++){
+            x2[i]=sc.nextInt();
+            if (x2[i]>maxx2)
+                maxx2=x2[i];
+        }
+        for (int i=0;i<n;i++){
+            y2[i]=sc.nextInt();
+            if (y2[i]>maxy2)
+                maxy2=y2[i];
+        }
+        int res = 0;
+//        for (int xx=minx1;xx<=maxx2;xx++){
+//            for (int yy=miny1;yy<=maxy2;yy++){
+//                int cur = 0;
+//                for (int i=0;i<n;i++){
+//                    if (helper(xx,yy,x1,y1,x2,y2,i)) {
+//                        cur++;
+//                    }
+//                }
+//                if (res<cur)
+//                    res=cur;
+//            }
+//        }
+        for (int i=0;i<n;i++){
+            for (int xx=x1[i]+1;xx<x2[i];xx++){
+                for (int yy=y1[i]+1;yy<y2[i];yy++){
+                    int cur = 0;
+                    for (int ii=0;ii<n;ii++){
+                    if (helper(xx,yy,x1,y1,x2,y2,ii)) {
+                        cur++;
+                    }
+                }
+                if (res<cur)
+                    res=cur;
+                }
+            }
+        }
+        if (res==0)
+            res=1;
+        System.out.println(res);
+    }
 
+    static boolean helper(int x,int y,int[] x1,int[] y1,int[] x2,int[] y2,int i){
+        return x1[i]<x&&y1[i]<y&&x2[i]>x&&y2[i]>y;
     }
 }
--- src/pastExamPaper/wangyi/wangyi2018test03.java
+++ src/pastExamPaper/wangyi/wangyi2018test03.java
@@ -8,5 +8,47 @@ import java.util.Scanner;
 public class wangyi2018test03 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int[] D = new int[N];
+        int[] P = new int[N];
+        int[] A = new int[M];
+        int[] res = new int[M];
+        for (int i=0;i<N;i++){
+            D[i]=sc.nextInt();
+            P[i]=sc.nextInt();
+        }
+        shellSort(D,P);
+        for (int i=0;i<M;i++){
+            A[i]=sc.nextInt();
+        }
+        for (int i=0;i<M;i++){
+            for (int j=0;j<N;j++){
+                if (D[j]>A[i])
+                    break;
+                if (P[j]>res[i])
+                    res[i]=P[j];
+            }
+        }
+        for (int i=0;i<M;i++){
+            System.out.println(res[i]);
+        }
+    }
+
+    public static void shellSort(int[] a,int[] b){
+        int i,j,k,k2;
+        for(int dk=a.length/2;dk>=1;dk=dk/2)
+            for(i=dk;i<a.length;i++){
+                if(a[i]<a[i-dk]){
+                    k=a[i];
+                    k2=b[i];
+                    for(j=i-dk;j>=0&&k<a[j];j-=dk){
+                        a[j+dk]=a[j];
+                        b[j+dk]=b[j];
+                    }
+                    a[j+dk]=k;
+                    b[j+dk]=k2;
+                }
+            }
     }
 }
